7610eeb32d895a2247087cb8a1d50e17f97b9092
Kang <rkdxorud3@naver.com>
2021-10-28 17:29:27 +0900

    10.28-17:29

--- 09_mysql.tf
+++ 09_mysql.tf
@@ -1,5 +1,5 @@
-resource "azurerm_mysql_server" "kang-sql" {
-  name                = "kang-sql"
+resource "azurerm_mysql_server" "kang-db-server" {
+  name                = "kang-db"
   location            = azurerm_resource_group.kang_RG.location
   resource_group_name = azurerm_resource_group.kang_RG.name
 
@@ -10,11 +10,27 @@ resource "azurerm_mysql_server" "kang-sql" {
   storage_mb = 5120
   version    = "5.7"
 
-  auto_grow_enabled                 = true
   backup_retention_days             = 7
+  auto_grow_enabled                 = false
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
+}
+
+resource "azurerm_mysql_database" "kang-db" {
+  name                = "db"
+  resource_group_name = azurerm_resource_group.kang_RG.name
+  server_name         = azurerm_mysql_server.kang-db-server.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+
+resource "azurerm_mysql_firewall_rule" "kang-db-fw" {
+  name                = "test-fw"
+  resource_group_name = azurerm_resource_group.kang_RG.name
+  server_name         = azurerm_mysql_server.kang-db-server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
 }
